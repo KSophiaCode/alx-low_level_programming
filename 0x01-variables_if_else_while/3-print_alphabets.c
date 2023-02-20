5610ba21f70de9db6841884c3aba5bf085af6673
Kosisochukwu <adimalikosisochukwu@outlook.com>
2023-02-19 19:06:30 -0800

    capital A

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prigram ebtrt point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= "Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
