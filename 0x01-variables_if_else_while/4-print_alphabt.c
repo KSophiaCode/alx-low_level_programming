ca2a80420fac7078940912bc830bf870ca551489
Kosisochukwu <adimalikosisochukwu@outlook.com>
2023-02-19 19:30:30 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e'; || i == 'q') continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
