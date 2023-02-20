42073c449aa7dae2020a02c39b6ae47f0e3c573b
Kosisochukwu <adimalikosisochukwu@outlook.com>
2023-02-19 18:56:24 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+		putchar('\n');
+		return (0);
+}
