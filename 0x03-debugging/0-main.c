ca2d4594b29a3a047d5f36706dfd5fa03b28b7c2
KSophiaCode <adimalikosisochukwu@outlook.com>
2023-02-23 08:08:28 -0800

    debugging 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#includede "main.h"
+/**
+ * main - tests function that prints if integer is positivr or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positve_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positive_or_negative(int i);
+largest = largest_number(a, b, c);
+void print_remaining_days(month, day, year);
+int convert_day(int month, int day);
+
+#endif
