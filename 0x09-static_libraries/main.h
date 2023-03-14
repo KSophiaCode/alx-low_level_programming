0965134992928f027ec35212623eac43e4000a30
KSophia <adimalikosisochukwu@outlook.com>
2023-03-14 05:51:38 -0700

    static libary

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-                
-int _putchar(char c);                
+
+int main(void);
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
