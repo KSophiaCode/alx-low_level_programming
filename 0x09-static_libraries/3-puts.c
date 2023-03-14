484da524c6213499df206c36173a9c4d216322ba
KSophia <adimalikosisochukwu@outlook.com>
2023-03-14 03:05:19 -0700

    static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -3,7 +3,7 @@ void _puts(char *str);
 #include<stdio.h>
 /**
 * main - Entry point
-*_put: void
+*_put: write the character c to stdout
 * Return: Always 0 (Success)
 */
 int main(void)
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+* putchar - write the character c to stdout
+* @c: The character to print
+* Return: 1 if true, 0 if false
+* on error: -1 return
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
