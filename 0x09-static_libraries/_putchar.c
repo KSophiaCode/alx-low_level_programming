de55ab7b21b189ddc06ef28ae1a425070386e57c
KSophia <adimalikosisochukwu@outlook.com>
2023-03-14 04:26:48 -0700

    static lib

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,11 +1,19 @@
-#include <unistd.h>
+#include "main.h"
+
 /**
-* putchar - write the character c to stdout
-* @c: The character to print
-* Return: 1 if true, 0 if false
-* on error: -1 return
+*main - Entry point
+*Return: nothing, return void
 */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
