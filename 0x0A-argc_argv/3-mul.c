0c8bc541f335ff2204dad1cf15e17cafbb73cb0a
KSophia <adimalikosisochukwu@outlook.com>
2023-03-14 06:13:43 -0700

    3 multiple

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 /**
-* main - prints the multiplication of two integers
-* @argc: argument count
-* @argv: argument vector
-* Return: 0 if true, 1 if false
+* main - Multipies two numbers
+* @argc: number of command line arguments
+* @argv: array that contains the program
+* Return: 0 - success
 */
 int main(int argc, char *argv[])
 {
-	int a, b;
+	int n, m, diff;
 
-	if (argc == 1)
-			{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-	printf("%d\n", a * b);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n = atoi(argv[1]);
+	m = atoi(argv[2]);
+	diff = n * m;
+
+	printf("%i\n", diff);
 	return (0);
-}
-printf("Error\n");
-return (1);
 }
