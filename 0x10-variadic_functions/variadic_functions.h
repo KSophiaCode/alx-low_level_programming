1373bdb4652deecdbf27b6f9134d73b07fdb6de1
KSophia <adimalikosisochukwu@outlook.com>
2023-03-24 17:23:25 -0700

    task 0 and task 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,20 @@
-#define VARIADIC_FUNCTIONS_H
 #ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/* function declarations go here */
+
+#include <stdarg.h>
+#include <stdio.h>
+
+/* function that returns the sum of all its parameters */
+
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char);
-int main(void);
+
+/* function that prints numbers, followed by a new line */
+
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif
+#endif /* VARIADIC_FUNCTIONS_H */
